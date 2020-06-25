441e237fce8c153a0b7ed2ca349ea0f77c7697e7
Rwanda S <rwanda.ace@gmail.com>
2020-06-25 15:37:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
+ configure do
+  enable :sessions
+  set :session_secret, "secret"
+  end
+
+
 class App < Sinatra::Base
+  
+ get '/' do
+      erb :index
+ end
+ post "/checkout" do
+   @sessions = {}
+   @sessions = sessions
+  @sessions["item"] = params[:item]
+  
+  @sessions["item"]
+ end
 end
